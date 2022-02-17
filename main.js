b1ae6ffb68af3efda477b0091957470a621f9db5
yaroslav <yaroslavpatcalon@gmail.com>
2022-02-17 11:59:18 +0200

    First version project.All function works

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict"
+
+let N;
+inputIntN(N);
+let M;
+inputIntM(M);
+let even = confirm("Пропускати парні?");
+let SUM = 0;
+for (let i = N; i <= M; i++) {
+   if (even == false) {
+      SUM += i;
+   } else {
+      if (i % 2 == 1) {
+         SUM += i;
+      }
+   }
+}
+if (even == true) {
+   console.log(`Сума всіх чисел без парних: ${SUM}`);
+} else {
+   console.log(`Сума всіх чисел: ${SUM}`);
+}
+function inputIntN() {
+   N = +prompt("Введіть ціле число N");
+   while (N % 1 !== 0) {
+      N = +prompt("ВИ ВВЕЛИ НЕ ЦІЛЕ ЧИСЛО!!!Введіть ціле число заново");
+   }
+}
+function inputIntM() {
+   M = +prompt("Введіть ціле число M");
+   while (M % 1 !== 0) {
+      M = +prompt("ВИ ВВЕЛИ НЕ ЦІЛЕ ЧИСЛО!!!Введіть ціле число заново");
+   }
+}
+
+
