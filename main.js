c763dec261142013db4a22add71d52ead7720dfa
yaroslav <yaroslavpatcalon@gmail.com>
2022-02-19 15:58:38 +0200

    optimaze

--- main.js
+++ main.js
@@ -3,21 +3,16 @@
 document.querySelector('.form__button').addEventListener('click', () => {
    const N = +document.getElementById('numberN').value;
    const M = +document.getElementById('numberM').value;
-   let check = document.querySelector('.checkbox__input').value;
-   if (document.querySelector('.checkbox__input').checked == false) {
-      check = "off";
-   }
+   let check = document.querySelector('.checkbox__input').checked;
    error = error();
-   if (error === 0 && N > M || N == M) {
-      alert("Неможливо порахувати суму!Введіть число N яке має бути меншим за М ");
-   } else {
+   if (error === 0) {
       let sum = 0;
       for (let i = N; i <= M; i++) {
-         if (check == "on" && i % 2 == 0)
+         if (check && i % 2 == 0)
             continue;
          sum += i;
       }
-      if (check == "on") {
+      if (check) {
          document.querySelector('.without-pairs').innerHTML += sum + " , ";
       } else {
          document.querySelector('.all-number ').innerHTML += sum + " , ";
@@ -30,6 +25,10 @@ document.querySelector('.form__button').addEventListener('click', () => {
          error++;
       } else if (M % 1 !== 0) {
          alert("ВИ ВВЕЛИ НЕ ЦІЛЕ ЧИСЛО M!!!Введіть ціле число заново")
+         error++;
+      }
+      if (N > M || N == M) {
+         alert("Неможливо порахувати суму!Введіть число N яке має бути меншим за М ");
          error++;
       }
       return error;
